7f341cb54247a3ce85a07253921e61ecbb3839a7
callumgoodley <callumgoodley@btinternet.com>
2020-06-12 14:04:41 +0000

    changes made to .gitignore

---
+++ application/forms.py
@@ -0,0 +1,118 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, PasswordField, BooleanField
+from application.models import Users
+from wtforms.validators import DataRequired, Length
+from flask_login import current_user
+
+class SongForm(FlaskForm):
+    title = StringField('title', 
+            validators = [
+                DataRequired(),
+                Length(min=2, max=30)
+                ]
+            )
+    artist = StringField('artist', 
+            validators = [
+                DataRequired(),
+                Length(min=2, max=30)
+                ]
+            )
+    album  = StringField('album',
+            validators = [
+                DataRequired(),
+                Length(min=2, max=30)
+                ]
+            )
+    genre = StringField('genre', 
+            validators = [
+                DataRequired(),
+                Length(min=2, max=30)
+                ]
+            )
+    year = StringField('year', 
+            validators = [
+                DataRequired(),
+                Length(min=4, max=4)
+                ]
+            )
+    submit = SubmitField('add song!')
+
+class RegistrationForm(FlaskForm):
+    
+    first_name = StringField('First Name',
+        validators = [
+            DataRequired(),
+            Length(min=2, max=30)
+        ]
+    )
+    last_name = StringField('Last Name',
+        validators = [
+            DataRequired(),
+            Length(min=2, max=30)
+        ]
+    )
+    email = StringField('Email',
+        validators = [
+            DataRequired(),
+            Email()
+        ]
+    )
+    password = PasswordField('Password',
+        validators = [
+            DataRequired(),
+        ]
+    )
+    confirm_password = PasswordField('Confirm Password',
+        validators = [
+            DataRequired(),
+            EqualTo('password')
+        ]
+    )
+    submit = SubmitField('Sign Up')
+
+    def validate_email(self, email):
+        user = Users.query.filter_by(email=email.data).first()
+
+        if user:
+            raise ValidationError('Email already in use')
+
+class LoginForm(FlaskForm):
+    email = StringField('Email',
+        validators=[
+            DataRequired(),
+            Email()
+        ]
+    )
+
+    password = PasswordField('Password',
+        validators=[
+            DataRequired()
+        ]
+    )
+
+    remember = BooleanField('Remember Me')
+    submit = SubmitField('Login')
+
+class UpdateAccountForm(FlaskForm):
+    first_name = StringField('First Name',
+        validators=[
+            DataRequired(),
+            Length(min=4, max=30)
+        ])
+    last_name = StringField('Last Name',
+        validators=[
+            DataRequired(),
+            Length(min=4, max=30)
+        ])
+    email = StringField('Email',
+        validators=[
+            DataRequired(),
+            Email()
+        ])
+    submit = SubmitField('Update')
+
+    def validate_email(self,email):
+        if email.data != current_user.email:
+            user = Users.query.filter_by(email=email.data).first()
+            if user:
+                raise ValidationError('Email already in use')
---
+++ application/models.py
@@ -0,0 +1,40 @@
+from application import db, login_manager
+from flask_login import UserMixin
+from datetime import datetime
+
+
+
+class Users(db.Model, UserMixin):
+    
+    @login_manager.user_loader
+    def load_user(id):
+        return Users.query.get(int(id))
+
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(30), nullable=False)
+    last_name = db.Column(db.String(30), nullable=False)
+    email = db.Column(db.String(150), nullable=False, unique=True)
+    password = db.Column(db.String(500), nullable=False)
+    songs = db.relationship('Songs', backref='added by', lazy=True)
+
+    def __repr__(self):
+        return ''.join([
+            'User ID: ', str(self.id), '\r\n',
+            'Email: ', self.email, '\r\n',
+            'Name: ', self.first_name, ' ', self.last_name
+        ])
+
+class Songs(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    artist = db.Column(db.String(100), nullable=False)
+    album = db.Column(db.String(100), nullable=False)
+    genre = db.Column(db.String(50), nullable=False)
+    year = db.Column(db.String(4), nullable=False)
+
+class Playlist(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    date_created = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+
